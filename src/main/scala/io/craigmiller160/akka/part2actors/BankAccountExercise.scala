06c2e5ddc41685302c91f7a213a149fd3efb00d8
Craig Miller <craigmiller160@gmail.com>
2022-02-26 16:12:41 -0500

    Working on deposit and withdraw

--- src/main/scala/io/craigmiller160/akka/part2actors/BankAccountExercise.scala
+++ src/main/scala/io/craigmiller160/akka/part2actors/BankAccountExercise.scala
@@ -4,6 +4,7 @@ package io.craigmiller160.akka.part2actors
 import akka.actor.{Actor, ActorSystem}
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
 
 object BankAccountExercise extends App {
   val actorSystem = ActorSystem("bankAccountSystem")
@@ -15,13 +16,20 @@ object BankAccountExercise extends App {
 case class Deposit(amount: Double)
 case class Withdraw(amount: Double)
 case class Statement()
+case class InsufficientFundsException(balance: Double) extends RuntimeException(s"Insufficient funds. Balance: $balance")
 
 class BankAccount extends Actor {
   var balance = 0
 
   override def receive: Receive = {
-    case Deposit(amount) => ???
-    case Withdraw(amount) => ???
+    case Deposit(amount) =>
+      balance += amount
+      Success(balance)
+    case Withdraw(amount) if (amount > balance) =>
+      Failure(new InsufficientFundsException(balance))
+    case Withdraw(amount) =>
+      balance -= amount
+      Success(balance)
     case Statement => ???
   }
 }
