3ba401deda867b223b880642be7092be0b60c05e
Craig Miller <craigmiller160@gmail.com>
2022-02-27 07:34:30 -0500

    Added pipe function

--- src/main/scala/io/craigmiller160/akka/part2actors/BankAccountExercise.scala
+++ src/main/scala/io/craigmiller160/akka/part2actors/BankAccountExercise.scala
@@ -8,6 +8,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import scala.util.chaining._
 
 // TODO the statement request is hitting dead letter
 
@@ -98,8 +99,9 @@ class AccountOwner(account: ActorRef) extends Actor {
       println(s"Request successful: Operation: $operation Amount: $amount Balance: $balance")
     case Success(Statement(timestamp, balance, transactions)) =>
       val txns = transactions.map(txn => s"Timestamp: ${Transaction.format(txn.timestamp)} Operation: ${txn.operation} Amount: ${txn.amount} Start Balance: ${txn.startBalance} End Balance: ${txn.endBalance}")
-      val txnString = Monoids.stringMonoid.combineAll(txns)
-      println(s"Statement Request successful. Timestamp: $timestamp Balance: $balance Transactions:\n$txnString")
+        .pipe(Monoids.stringMonoid.combineAll)
+//      val txnString = Monoids.stringMonoid.combineAll(txns)
+      println(s"Statement Request successful. Timestamp: $timestamp Balance: $balance Transactions:\n$txns")
     case Failure(ex: BankAccountException) =>
       println(s"Request failed: ${ex.getMessage}")
   }
