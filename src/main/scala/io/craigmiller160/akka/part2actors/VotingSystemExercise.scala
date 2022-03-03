3008d4709b871b8f2c674ce9b72205434f86a936
Craig Miller <craigmiller160@gmail.com>
2022-03-03 13:38:38 -0500

    Wrote aggregator

--- src/main/scala/io/craigmiller160/akka/part2actors/VotingSystemExercise.scala
+++ src/main/scala/io/craigmiller160/akka/part2actors/VotingSystemExercise.scala
@@ -23,8 +23,27 @@ case class VoteAggregation(billClinton: Int, georgeBush: Int, rossPerot: Int)
 case class AggregateVotes(citizens: Set[ActorRef])
 
 class Citizen extends Actor {
-  override def receive: Receive = ???
+  override def receive: Receive = doReceive(None)
+
+  private def doReceive(voteChoice: Option[Candidate.Value]): Receive = {
+    case Vote(candidate) => voteChoice match {
+      case Some(_) => // Do nothing
+      case None => context.become(doReceive(Some(candidate)))
+    }
+    case VoteStatusRequest => sender() ! VoteStatusReply(voteChoice)
+  }
 }
 class VoteAggregator extends Actor {
-  override def receive: Receive = ???
+  override def receive: Receive = doReceive(VoteAggregation(0, 0, 0))
+
+  // TODO what if multiple AggregateVotes requests are sent?
+  private def doReceive(aggregation: VoteAggregation): Receive = {
+    case AggregateVotes(citizens) =>
+      citizens.foreach(ref => ref ! VoteStatusRequest)
+    case VoteStatusReply(candidate) => candidate match {
+      case Candidate.BILL_CLINTON => context.become(doReceive(aggregation.copy(billClinton = aggregation.billClinton + 1)))
+      case Candidate.GEORGE_BUSH => context.become(doReceive(aggregation.copy(georgeBush = aggregation.georgeBush + 1)))
+      case Candidate.ROSS_PEROT => context.become(doReceive(aggregation.copy(rossPerot = aggregation.rossPerot + 1)))
+    }
+  }
 }
