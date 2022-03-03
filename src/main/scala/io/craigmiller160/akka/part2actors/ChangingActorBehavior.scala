6ac54d55d002904a8d8e2af2f270a93c16dc0b6b
Craig Miller <craigmiller160@gmail.com>
2022-03-03 10:37:13 -0500

    More work on new actors

--- src/main/scala/io/craigmiller160/akka/part2actors/ChangingActorBehavior.scala
+++ src/main/scala/io/craigmiller160/akka/part2actors/ChangingActorBehavior.scala
@@ -10,14 +10,29 @@ object ChangingActorBehavior extends App {
 object FussyKid {
   case object KidAccept
   case object KidReject
+  val HAPPY = "happy"
+  val SAD = "sad"
 }
 class FussyKid extends Actor {
-  override def receive: Receive = ???
+  import FussyKid._
+  var state: String = HAPPY
+  override def receive: Receive = {
+    case Mom.Food(food) => food match {
+      case Mom.VEGETABLE => state = SAD
+      case Mom.CHOCOLATE => state = HAPPY
+    }
+    case Mom.Ask => state match {
+      case HAPPY => sender() ! KidAccept
+      case SAD => sender() ! KidReject
+    }
+  }
 }
 
 object Mom {
   case class Food(food: String)
   case class Ask(message: String)
+  val VEGETABLE = "veggies"
+  val CHOCOLATE = "chocolate"
 }
 class Mom extends Actor {
   override def receive: Receive = ???
